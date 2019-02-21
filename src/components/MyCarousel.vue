4c685a5678bd4c5492f8a912ba837898b9f6d1ba
pumy2517 <60070067@kmitl.ac.th>
2019-02-21 22:46:32 +0700

    upupup

--- src/components/MyCarousel.vue
+++ src/components/MyCarousel.vue
@@ -2,17 +2,18 @@
   <div class="">
     <carousel :perPage="1" :paginationEnabled="false" :autoplayHoverPause="false" :autoplay="true" :loop="true"
       :autoplayTimeout="5000">
-      <slide v-for="wallpapers,index_img in movies">
+      <slide v-for="movie,index_img in movies">
         <div class="movie">
-          <div class="size_carousel" :style="{'background-image':'url('+wallpapers.wallpaper+')'}">
+          <div class="size_carousel" :style="{'background-image':'url('+movie.wallpaper+')'}">
             <div class="container">
               <div class="row">
                 <div class="col-md-9 text-left text-bg" >
-                  <h1>{{wallpapers.name.en}}</h1>
-                  <h4>{{wallpapers.name.th}}</h4>
+                  <h1>{{movie.name.en}}</h1>
+                  <h4>{{movie.name.th}}</h4>
                   <h3  >
                     หมวดหมู่:
-                  <label v-for="categorie,index in categories" v-if="categorie.name[index] == wallpapers.categorie"> &nbsp;{{categorie.name}} |</label>
+                  <label v-for="categorie in movie.categories" > &nbsp;{{categories[categorie]}} |</label>
+                  <!-- <label v-for="categorie,index in categories" v-if="categorie.name[index] == movie.categorie"> &nbsp;{{categorie.name}} |</label> -->
                 </h3>
                   <a href="/showtime/movie/HO00000453" class="btn btn-primary btn-lg" title="คุซามะ อินฟินิตี้">
                     รายละเอียดหนัง</a>
