e1b9e1c905da98b8789b731796943e81bb0ed721
pumy2517 <60070067@kmitl.ac.th>
2019-02-21 22:26:26 +0700

    Add_banner_carousel

--- src/components/MyCarousel.vue
+++ src/components/MyCarousel.vue
@@ -1,12 +1,31 @@
 <template>
-  <div class="" >
-    <carousel :perPage="1" :paginationEnabled="false" :autoplayHoverPause="false" :autoplay="true" :loop="true" :autoplayTimeout="5000">
-      <slide v-for="wallpapers in movies">
-        <div class="size_carousel" :style="{'background-image':'url('+wallpapers.wallpaper+')'}"></div>
+  <div class="">
+    <carousel :perPage="1" :paginationEnabled="false" :autoplayHoverPause="false" :autoplay="true" :loop="true"
+      :autoplayTimeout="5000">
+      <slide v-for="wallpapers,index_img in movies">
+        <div class="movie">
+          <div class="size_carousel" :style="{'background-image':'url('+wallpapers.wallpaper+')'}">
+            <div class="container">
+              <div class="row">
+                <div class="col-md-9 text-left text-bg" >
+                  <h1>{{wallpapers.name.en}}</h1>
+                  <h4>{{wallpapers.name.th}}</h4>
+                  <h3  >
+                    หมวดหมู่:
+                  <label v-for="categorie,index in categories" v-if="categorie.name[index] == wallpapers.categorie"> &nbsp;{{categorie.name}} |</label>
+                </h3>
+                  <a href="/showtime/movie/HO00000453" class="btn btn-primary btn-lg" title="คุซามะ อินฟินิตี้">
+                    รายละเอียดหนัง</a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
         <!-- <img class=""alt=""> -->
       </slide>
     </carousel>
-    
+
   </div>
 </template>
 
@@ -14,7 +33,7 @@
 
   export default {
     name: 'MyCarousel',
-    props: ['movies'],
+    props: ['movies', 'categories'],
   }
 
 </script>
@@ -25,5 +44,13 @@
     background-repeat: no-repeat;
     background-size: cover;
     background-position: center;
+  }
+
+  .text-bg {
+    position: absolute;
+    bottom: 10%;
+    color: white;
+    /* background-color: wheat */
+    opacity: 1;
   }
 </style>
