6dbb204c6154b7c9ee1c7ff57a9f4f0cdcffaa1c
pumy2517 <60070067@kmitl.ac.th>
2019-02-21 20:21:21 +0700

    Logo and navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <nav class="navbar navbar-expand-lg navbar-light bg-primary ">
-      <a class="navbar-brand" href="#">Navbar</a>
+    <nav class="navbar navbar-expand-lg  navbar-light bg-light ">
+      <a href="#" class="navbar-left"><img src="../assets/img/logo.png" img style="width: 150px;"></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
