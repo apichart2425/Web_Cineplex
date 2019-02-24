ec6b51332e694cf60f577b72ef16c10f53c4bcf6
pumy2517 <60070067@kmitl.ac.th>
2019-02-24 23:49:24 +0700

    seat

--- src/components/movie.vue
+++ src/components/movie.vue
@@ -1,69 +1,125 @@
 <template>
+  <!-- <div class="row">
+      <div class="col-9">Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore alias tenetur beatae fugit
+        pariatur, delectus odit ducimus accusantium suscipit blanditiis sit cum expedita. Soluta facere ducimus possimus
+        deserunt nesciunt suscipit.</div>
+      <div class="col-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem maiores odit ipsa explicabo
+        dolorum qui? Velit, iste! Hic eum exercitationem non, ducimus esse molestias ea nobis, voluptatum doloremque
+        repellat quis.</div>
+    </div> -->
   <div class="theater">
-    <div class="container">
-      <div class="row">
-        <div class="col-sm-9">
-          <div class="seat">
-            <table class="table-seat" style="width: 100%; position: relative;">
-              <tbody>
-                <tr>
-                  <td style="height: 30px; width: 30px;"></td>
-                  <td colspan="20">
-                      <div class="screen-image" style="margin-bottom: 20px"><img src="../assets/img/screen.svg" alt=""></div>
-                  </td>
-                </tr>
-                <tr v-for="chair in seat">
-                  <td style="text-align: center; padding: 0 0 0 0;">{{chair.id}}</td>
-                  <td style="height: 30px; width: 30px;"></td>
-                  <td v-for="chair_no in chair.seat_no" :class="chair_no" style="margin-bottom: 0; padding: 6px 0 0 0;">
-                    <input type='checkbox' :name='chair_no' value='valuable' :id="chair_no" /><label :for="chair_no"
-                      style="margin-bottom: 0;"></label>
-                  </td>
-                  <td style="height: 30px; width: 30px;"></td>
-                  <td style="text-align: center; padding: 0 0 0 0;">{{chair.id}}</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
+    <!-- <div class="container"> -->
+    <div class="row" style="margin-left: 10%; margin-right: 10%">
+      <div class="col-lg-9">
+        <div class="seat">
+          <table class="table-seat" style="width: 100%; position: relative;">
+            <tbody>
+              <td style="height: 30px; width: 60px;"></td>
+              <td colspan="20">
+                <div class="screen-image" style="margin-bottom: 20px"><img src="../assets/img/screen.svg" alt=""></div>
+              </td>
+              </tr>
+              <tr v-for="chair in seat">
+                <td style="text-align: center; padding: 0 0 0 0;">{{chair.id}}</td>
+                <td style="height: 30px; width: 30px;"></td>
+                <td v-for="chair_no in chair.seat_no" :class="chair_no" style="margin-bottom: 0; padding: 6px 0 0 0;">
+                  <input type="checkbox" :id="chair_no" :value="chair_no" v-model="seatName">
+                  <label :for="chair_no"></label>
+                </td>
+                <td style="height: 30px; width: 60px;"></td>
+                <td style="text-align: center; padding: 0 0 0 0;">{{chair.id}}</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
-        <div class="col-sm-3">
-          <p>{{seat[0].seat_no}}</p>
+      </div>
+      <div class="col-lg-3">
+        <div class="book-seat">
+          <div class="logo">
+            <img src="../assets/img/logotext.png" alt="" style="width: 100%">
+          </div>
+          <div class="movie-detail">
+            <h3 class="movie-name"><b>{{movies[0].name.en}}</b></h3>
+            <h5 style="padding-top: 2%">&nbsp;&nbsp;&nbsp;<b>6 March 2019</b></h5>
+          </div>
+          <div class="summary">
+            <div class="price">
+              <div class="select" style="text-align: center">
+                <h3>ที่นั่งที่เลือก</h3>
+                <div class="selected-seat">
+                  <h2><b>{{seatName}}</b></h2>
+                </div>
+              </div>
+              <div class="total-price">
+                <h3 style="margin-top: 5%; text-align: center">ราคารวม</h3>
+                <h2 style="text-align: center"><b>{{ seatName.length*120 }}</b></h2>
+              </div>
+              <div class="button" style="text-align: center; margin-top: 10%">
+                <button type="button" class="btn btn-primary" style="width: 50%; height: 100%;">Success</button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
+    <!-- </div> -->
   </div>
 </template>
 
 <script>
+
   export default {
     data() {
       return {
+        seatName: [],
+        movies: movies,
+        categories: categories,
         seat: seats,
+        selectseat: selectseat
       }
     }
   }
+
   function addSeat(seat_id) {
+
   }
 </script>
 
 <style scoped>
+  .summary {
+    margin-top: 20%;
+    background-color: #fff;
+    padding: 20px;
+  }
+
+  .book-seat {
+    background-color: #f5f7fb;
+    padding: 30px;
+    text-align: left
+  }
+
+  .movie-name {
+    margin-top: 40px;
+  }
+
   input[type=checkbox] {
     display: none;
   }
+
   input[type=checkbox]+label {
     display: inline-block;
     background: url(http://www.majorcineplex.com/assets/front/images/booking2/seat-2.png) no-repeat center top transparent;
     background-size: contain;
-    width: 35px;
+    width: 40px;
     height: 35px;
     padding: 0 0 0 0;
   }
+
   input[type=checkbox]:checked+label {
     background: url(http://www.majorcineplex.com/assets/front/images/booking2/seatsuccessSym.png) no-repeat center top transparent;
     height: 35px;
-    width: 35px;
+    width: 40px;
     display: inline-block;
     padding: 0 0 0 0;
   }
-  
 </style>
